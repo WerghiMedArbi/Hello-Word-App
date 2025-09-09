5aab8e1fbeb12820efb1e670049dc41c22b6f3e2
werghimedarbi <werghimedarbi@gmail.com>
2025-09-09 11:16:54 +0100

    trying workflowhhhhhhhhh

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello Maddoussa, it stands for MADD and Oussama who is really annoying :) !'
+    return 'Hello Maddoussa, it stands for MADD and Oussama :) !'
  
 # main driver function
 if __name__ == '__main__':
